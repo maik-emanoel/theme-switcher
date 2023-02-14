7d15dca88264e113407cdd42e166d9ff5ccb8d0e
Maik Emanoel <105499887+maik-emanoel@users.noreply.github.com>
2023-02-14 20:02:25 -0300

    code improvement

--- script.js
+++ script.js
@@ -11,14 +11,12 @@ toggle.addEventListener('click', function darkMode() {
   ball.classList.toggle('black')
   p.classList.toggle('black')
   link.classList.toggle('black')
-
-  const isDarkModeOn = h2.classList.toggle('black')
   
-  if( isDarkModeOn === true) {
+  if(body.classList.contains('black')) {
     h2.innerHTML = 'Hello there, DarkModeON!'
-    h2.style.transition = '.4s ease-in-out'
+    h2.classList.add('black')
   } else {
     h2.innerHTML = 'Hello there, LightModeON!'
+    h2.classList.remove('black') 
   }
-
 })
